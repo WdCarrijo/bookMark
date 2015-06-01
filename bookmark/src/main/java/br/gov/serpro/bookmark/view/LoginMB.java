aa8909728c6b240ebf452f03827338b1e6aea15b
Welder <welder.carrijo@gmail.com>
2015-06-01 16:42:09 -0300

    Classe criada para controle de navegacao e autenticar usuario.

---
+++ bookmark/src/main/java/br/gov/serpro/bookmark/view/LoginMB.java
@@ -0,0 +1,58 @@
+package br.gov.serpro.bookmark.view;
+
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.annotation.NextView;
+import br.gov.frameworkdemoiselle.message.MessageContext;
+import br.gov.frameworkdemoiselle.security.Credentials;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
+import br.gov.frameworkdemoiselle.stereotype.ViewController;
+import br.gov.frameworkdemoiselle.template.AbstractPageBean;
+
+@ViewController
+@NextView("./index.jsf")
+public class LoginMB extends AbstractPageBean {
+	private static final long serialVersionUID = 1L;
+
+	private String usuario = new String();
+	private String senha = new String();
+
+	@Inject
+	Credentials credentials;
+	@Inject
+	SecurityContext securityContext;
+	@Inject
+	MessageContext menssageContext;
+
+	public String doLogin() {
+		try {
+			credentials.setUsername(this.getUsuario());
+			credentials.setPassword(this.senha);
+			securityContext.login();
+			return getNextView();
+		} catch (Exception e) {
+			menssageContext.add(e.getMessage());
+			return "";
+		}
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void doLogout() {
+		securityContext.logout();
+	}
+}
